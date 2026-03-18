96380035818f9b18f1bb78a6949b4b394e4f34e7
Ninad Gawade <118633122+Ninad234@users.noreply.github.com>
2026-03-19 01:18:19 +0530

    Update API endpoint for product retrieval

--- Frontend/src/components/LatestCollection.jsx
+++ Frontend/src/components/LatestCollection.jsx
@@ -16,7 +16,7 @@ const LatestCollection = () => {
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/product")
+        const res = await axios.get("https://monoro-backend.onrender.com/product")
         const data2 = res.data.filter((item)=> item.category === "Flat 28% OFF")
         console.log(data2)
         setProducts(data2)
